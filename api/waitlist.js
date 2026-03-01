ba1e8ddeaa29aa8183ee3e2b2d37993cd118434a
jadhavca <jadhavca@gmail.com>
2026-03-01 10:48:22 +0000

    Update waitlist.js

--- api/waitlist.js
+++ api/waitlist.js
@@ -22,8 +22,8 @@ export default async function handler(req, res) {
       .insert([{ email, exam }])
 
     if (error) {
-      return res.status(400).json({ error: 'Already signed up' })
-    }
+    return res.status(200).json({ success: true })
+  }
 
     // Send confirmation email
     await resend.emails.send({
